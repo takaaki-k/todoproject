b43ef59affbe8e8b78ba1a9fd32cbd085c587c69
takaaki-k <121436318+takaaki-k@users.noreply.github.com>
2023-07-28 20:46:36 +0900

    detail page

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,5 +4,5 @@ from .views import TaskList, TaskDetail
 
 urlpatterns = [
   path("", TaskList.as_view()),
-  path("task/<int:pk>/", TaskDetail.as_view())
+  path("task/<int:pk>/", TaskDetail.as_view(),name="task")
 ]
