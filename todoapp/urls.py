82558ac5bd9497e3dad85b02e44e681388ecd7d1
takaaki-k <121436318+takaaki-k@users.noreply.github.com>
2023-07-30 21:03:15 +0900

    user authority add

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, TaskListLoginView, RegisterTodoApp
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
   path("", TaskList.as_view(), name="tasks"),
@@ -8,4 +9,7 @@ urlpatterns = [
   path("create-task", TaskCreate.as_view(),name="create-task"),
   path("edit-task/<int:pk>/", TaskUpdate.as_view(),name="edit-task"),
   path("delete-task/<int:pk>/", TaskDelete.as_view(),name="delete-task"),
+  path("login/", TaskListLoginView.as_view(), name="login"),
+  path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
+  path("register", RegisterTodoApp.as_view(), name="register"),
 ]
